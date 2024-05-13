77c9651f0a604feac788c71ec0b1c36fbc8e00e2
dfigueiredo1983 <docarmorama@gmail.com>
2024-05-12 21:58:47 -0300

    Validação da quantidade de linhas

--- rainhas.cpp
+++ rainhas.cpp
@@ -24,6 +24,12 @@ int valida_arquivo_entrada(const std::string &filename)
       return -1;
   }
 
+  return 0;
+}
+
+int valida_linhas(const std::string &filename)
+{
+  ifstream file(filename);
   string line;
   int nLine = 0;
   while (getline(file, line))
@@ -37,18 +43,14 @@ int valida_arquivo_entrada(const std::string &filename)
   return 0;
 }
 
-int game(const std::string &filename)
-{
-
-  return 0;
-}
-
 int rainhas(const std::string &filename)
 {
-
   int valida_arquivo = valida_arquivo_entrada(filename);
-
   if (valida_arquivo == -1)
+    return -1;
+
+  int linhas = valida_linhas(filename);
+  if (linhas == -1)
     return -1;
 
   return 0;
