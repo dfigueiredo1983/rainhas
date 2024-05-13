8c04b5732ddf76273bfc6178ade72dc278c0432b
dfigueiredo1983 <docarmorama@gmail.com>
2024-05-13 09:09:15 -0300

    Ajuste do projeto para validar quantidade de linhas e colunas

--- rainhas.cpp
+++ rainhas.cpp
@@ -15,6 +15,8 @@ int valida_arquivo_entrada(const std::string &filename)
     return -1;
 
   char caractere;
+  int numColunas = 0;
+  int numLinhas = 0;
   while (file.get(caractere))
   {
     if (isalpha(caractere))
@@ -22,33 +24,25 @@ int valida_arquivo_entrada(const std::string &filename)
 
     if (isdigit(caractere) && !(caractere == '0' || caractere == '1'))
       return -1;
+
+    if (caractere == '\n')
+    {
+      if (numColunas != 8)
+        return -1;
+
+      numColunas = 0;
+      numLinhas++;
+    }
+    else
+    {
+      ++numColunas;
+    }
   }
 
-  return 0;
-}
-
-int valida_linhas_colunas(const std::string &filename)
-{
-  ifstream file(filename);
-  string line;
-  int nLine = 0;
-  while (getline(file, line))
-  {
-    if (line.length() != 8)
-      return -1;
-    nLine++;
-  }
-
-  if (nLine != 8)
+  if (numLinhas != 7)
     return -1;
 
-  return 0;
-}
-
-int game(const std::string &filename)
-{
-
-  ifstream file(filename);
+  file.close();
 
   return 0;
 }
@@ -57,10 +51,6 @@ int rainhas(const std::string &filename)
 {
   int valida_arquivo = valida_arquivo_entrada(filename);
   if (valida_arquivo == -1)
-    return -1;
-
-  int linhas = valida_linhas_colunas(filename);
-  if (linhas == -1)
     return -1;
 
   return 0;
--- rainhas.hpp
+++ rainhas.hpp
@@ -2,5 +2,3 @@
 
 int rainhas(const std::string &filename);
 int valida_arquivo_entrada(const std::string &filename);
-int valida_linhas_colunas(const std::string &filename);
-int game(const std::string &filename);
