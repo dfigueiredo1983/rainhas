09e5a1aea2c00c0708e4a8b009eebea166afa664
dfigueiredo1983 <docarmorama@gmail.com>
2024-05-13 13:36:24 -0300

    Validando número de rainhas por linha

--- rainhas.cpp
+++ rainhas.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -47,10 +48,57 @@ int valida_arquivo_entrada(const std::string &filename)
   return 0;
 }
 
+int game(const std::string &filename)
+{
+  vector<int> tabuleiro;
+
+  ifstream file(filename);
+  if (!file)
+    return -1;
+
+  char caractere;
+  while (file.get(caractere)) // Converte as linhas do arquivo em um array de inteiros
+  {
+    if (caractere != '\n')
+    {
+      tabuleiro.push_back(caractere - '0');
+    }
+  }
+
+  file.close();
+
+  int numRainhasLinha = 0;
+  for (size_t i = 0; i < tabuleiro.size(); i++)
+  {
+
+    cout << tabuleiro.at(i);
+    if (tabuleiro.at(i) == 1)
+      numRainhasLinha++;
+
+    if (numRainhasLinha > 1)
+    {
+      cout << "Linha com mais de 1 rainha: " << numRainhasLinha << endl;
+      return -1;
+    }
+
+    if (i % 7 == 0)
+    {
+      numRainhasLinha = 0;
+      cout << endl;
+    }
+  }
+
+  return 0;
+}
+
 int rainhas(const std::string &filename)
 {
   int valida_arquivo = valida_arquivo_entrada(filename);
   if (valida_arquivo == -1)
+    return -1;
+
+  int valida_game = game(filename);
+  if (valida_game == -1)
     return -1;
 
   return 0;
